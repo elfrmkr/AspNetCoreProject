dc3d19005d7d83bcba2dc3b9bc2280b97f41f4b1
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2023-10-23 15:19:30 +0200

    feat: added example middleware with condition

--- AspNetCoreProject/Program.cs
+++ AspNetCoreProject/Program.cs
@@ -8,54 +8,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
 
-//app.Run(async (HttpContext context) =>
-//{
-//    System.IO.StreamReader reader = new StreamReader(context.Request.Body);
-//    string body = await reader.ReadToEndAsync();
-//    //parsing string to dictionary, keys should be unique but can have multiple values
-//    Dictionary<string, StringValues> queryDict =
-//    Microsoft.AspNetCore.WebUtilities.QueryHelpers.ParseQuery(body);
-
-//    if(queryDict.ContainsKey("firstName"))
-//    {
-//        string firstName = queryDict["firstName"][0];
-//        await context.Response.WriteAsync(firstName);
-//    }
-//});
-
 
 /*********************************************/
+// conditional middleware
+app.UseWhen(context =>
 
-//// this is a middleware
-//app.Run(async (HttpContext context) => {
-//    await context.Response.WriteAsync("Hello");
-//});
+context.Request.Query.ContainsKey("username"),
+    app =>
+    {
+        app.Use(async (context, next) =>
+        {
+            await context.Response.WriteAsync("Hello from" +
+            " middleware branch");
+            await next();
 
-//// second middleware doesnt execute because of the method used
-//// to form middleware (Run)
-//// Run doesn't execute chain middleware
-//app.Run(async (HttpContext context) => {
-//    await context.Response.WriteAsync("Hello again");
-//});
+        });
+    });
 
-/*********************************************/
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Hello from" +
+        " middleware at main chain");
 
-// middleware 1, takes two arguments
-app.Use(async (HttpContext context, RequestDelegate next) =>
-{
-    await context.Response.WriteAsync("Middleware 1\n");
-    await next(context); //calling the next middleware
-});
-
-//middleware 2
-//app.UseMiddleware<MyCustomMiddleware>();
-//app.UseMyCustomMiddleware();
-app.UseHelloCustomMiddleware();
-
-//middleware 3, takes only one argument
-app.Run(async (HttpContext context) => {
-    await context.Response.WriteAsync("Middleware 3\n");
-});
+    });
 
 
 app.Run(); // start to server
