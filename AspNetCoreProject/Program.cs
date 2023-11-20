e71dbf6b2973a773485fc0281d7e04955935ebca
Elif İrem Kara Kadyrov <83959541+elfrmkr@users.noreply.github.com>
2023-11-20 11:11:12 +0100

    feat: formed two middlewares to see end points

--- AspNetCoreProject/Program.cs
+++ AspNetCoreProject/Program.cs
@@ -1,28 +1,49 @@
 using AspNetCoreProject.CustomMiddleware;
-using Microsoft.Extensions.Primitives;
-using System.IO;
 
 //Builder loads the configuration, environment and default services
 var builder = WebApplication.CreateBuilder(args);
-// add middleware
-builder.Services.AddTransient<MyCustomMiddleware>();
 var app = builder.Build();
+
+app.Use(async (context, next) =>
+{
+   Microsoft.AspNetCore.Http.Endpoint? endpoint = context.GetEndpoint();
+    if(endpoint != null)
+    {
+       await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
+    }
+    await next(context);
+});
 
 // routing example
 app.UseRouting(); // enable routing
+
+
+app.Use(async (context, next) =>
+{
+    Microsoft.AspNetCore.Http.Endpoint? endpoint = context.GetEndpoint();
+    if (endpoint != null)
+    {
+        await context.Response.WriteAsync($"Endpoint: {endpoint.DisplayName}\n");
+    }
+    await next(context);
+});
 // creating endpoints
 app.UseEndpoints(endpoints =>
 {
     // add your end point, Map METHODS
-    endpoints.Map("/map1", async (context) =>
+    endpoints.MapGet("/map1", async (context) =>
     {
         await context.Response.WriteAsync("In map 1");
     });
-    endpoints.Map("/map2", async (context) =>
+    endpoints.MapPost("/map2", async (context) =>
     {
         await context.Response.WriteAsync("In map 2");
     });
 });
 
-
+app.Run(async context =>
+{
+    await context.Response.WriteAsync($"Request received at" +
+        $"{context.Request.Path}");
+});
 app.Run(); // start to server
